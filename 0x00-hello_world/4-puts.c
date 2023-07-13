d264b3285481321ebde1cc6da35c6061ef87b687
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-07-13 13:53:07 -0700

    hello puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include<stdio.h>
+/**
+ * main -entry point
+ *
+ * Return:Always 0 (Success)
+ */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+	char string [] "\"Programming is like building a multilingual puzzle";
+	puts(string)
 	return(0);
 }
