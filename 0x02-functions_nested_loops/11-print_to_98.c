ce5e1d483045ff5da66533b9905c92e751279b3a
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-07-19 04:23:06 -0700

    print to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -8,33 +8,21 @@
  */
 void print_to_98(int n)
 {
+	int i;
+
 	if (n <= 98)
 	{
-		for (; n <= 98; n++)
+		for (i = n; i < 98; i++)
 		{
-			if (n == 98)
-			{
-				printf("%d", n);
-				printf("\n");
-				break;
-			}
-			else
-			{
-				printf("%d, ", n);
-			}
+			printf("%d, ", i);
 		}
 	}
 	else
 	{
-		if (n == 98)
+		for (i = n; i > 98; i--)
 		{
-			printf("%d", n);
-			printf("\n");
-			break;
-		}
-		else
-		{
-			printf("%d, ", n);
+			printf("%d, ", i);
 		}
 	}
-
+	printf("98\n");
+}
