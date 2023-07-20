0bd4bebeace6b0b47b4204337a3ecfe7cebab4fc
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-07-20 02:26:00 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks for uppercase character
+ * @c: Number to be checked
+ * Return: 1 if c is uppercase or 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+		return (1);
+	}
+	return (0);
+}
