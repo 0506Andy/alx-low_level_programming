96f299f4b9a5f43fc88c6ffe4df1d473f3f4c623
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-07-24 13:49:52 -0700

    Half the lies they tell about me aren't true

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * puts2 - Prints every other character on a string
+ * @str: Character of string to be printed
+ * Return: String
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+		_putchar('\n');
+	}
+}
