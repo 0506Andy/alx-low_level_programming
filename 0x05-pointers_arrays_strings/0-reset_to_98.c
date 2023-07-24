057a101f3e9951c082a9f149989f70de962688c4
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-07-24 07:33:25 -0700

    98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ * reset_to_98 - takes a pointer as a parameter and updates a value
+ *
+ * @n: Number to be updated
+ *
+ * Return:void
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
