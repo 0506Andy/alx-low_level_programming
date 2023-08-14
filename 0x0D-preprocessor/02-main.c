8fde7f69e7dd81d1450158ec324cb467a6b1ce43
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-08-14 07:30:40 -0700

    File name

--- 0x0D-preprocessor/02-main.c
+++ 0x0D-preprocessor/02-main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-#define MAIN_C
-
 /**
- * main - Prints name of file it was colpiled from
+ * main -Prints name of file it was compiled from
  * REturn:0(success)
  */
 int main(void)
 {
-printf("%s\n", __FILE__);
-return (0);
+	printf("%s\n", __FILE__);
+	return (0);
 }
