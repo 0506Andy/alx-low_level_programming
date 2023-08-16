9e93b09a4292a5270247d25d1d482aa4a8f57a94
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-08-16 04:47:26 -0700

    What's my name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,18 @@
+#include "function_pointers.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_name - prints name
+ * @name: pointer name to string to add
+ * @f: pointer to function that prints name
+ * Return:void
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+		return;
+
+	f(name);
+}
+
