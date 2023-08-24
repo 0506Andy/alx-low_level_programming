64fdabcf657ee93d648a1dce5f8fe0f8deb922d3
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-08-24 08:02:51 -0700

    Add node

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,33 @@
+#include "lists.h"
+
+/**
+ * add_node - adds new node at the beginning of lists_t list
+ * @head:pointer to the first node
+ * @str:pointer to string to be dublicated
+ * Return:address of new element otherwise NULL
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	if (str == NULL)
+		return (NULL);
+
+	new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->str = strdup(str);
+
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->len = strlen(str);
+	new_node->next = *head;
+	*head = new_node;
+
+	return (new_node);
+}
