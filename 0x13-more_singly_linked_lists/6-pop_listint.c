2d0637dd0c3ba0fc5c2324341ce494f2fc90b45d
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-08-29 05:41:04 -0700

    Pop

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -11,15 +11,12 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 
 	if (head == NULL)
-	{
 		return (0);
-	}
-	else
-	{
-		temp = *head;
-		*head = (*head)->next;
-		data = temp->n;
-		free(temp);
-	}
+
+	temp = *head;
+	*head = (*head)->next;
+	data = temp->n;
+	free(temp);
+
 	return (data);
 }
