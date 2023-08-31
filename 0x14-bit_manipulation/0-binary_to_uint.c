c8e11810113ac72ac083f2c6c5dcff708b4d45fc
Thandi Andile Ndlovu <05thandy@gmail.com>
2023-08-31 05:04:23 -0700

    Binary to uint

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - converts binary to unit
+ * @b: pointer to string to be converted
+ * Return: converted number or 0 or 1
+ */
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int result;
+	unsigned int power;
+	int i;
+
+	result = 0;
+	power = strlen(b) - 1;
+
+	if (b == NULL)
+		return (0);
+
+	for (i = 0; b[i] != ('\0'); i++)
+	{
+		if (b[i] == ('1'))
+		{
+			result += (1 << power);
+		}
+		else if (b[i] != ('0'))
+		{
+			return (0);
+		}
+		power--;
+	}
+	return (result);
+}
