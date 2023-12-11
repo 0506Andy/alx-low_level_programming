67e7e6dd5e3e628e4a49a9e66720d2de06591c98
0506Andy <05thandy@gmail.com>
2023-12-11 04:03:20 -0800

    OPERATION

---
+++ 0x18-dynamic_libraries/addi.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * add - adds two integers
+ * @a:first int
+ * @b : second int
+ * Return:result
+ */
+int add(int a, int b)
+{
+	int result = a + b;
+
+	return (result);
+}
---
+++ 0x18-dynamic_libraries/mult.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+
+/**
+ * mul - multiplies two integers
+ * @a:first int
+ * @b : second int
+ * Return:result
+ */
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
